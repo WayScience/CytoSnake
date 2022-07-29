f19fd08c60651ece8d6449a91f21b47b6792211a
axiomcura <erikishere3@gmail.com>
2022-07-28 19:47:52 -0600

    Updated merge_logs script

--- scripts/merge_logs.py
+++ scripts/merge_logs.py
@@ -1,4 +1,9 @@
+import glob
+from pathlib import Path
+from datetime import datetime
+
 import pandas as pd
+
 
 def extract_logs(log_file_paths: list[str]) -> list[str]:
     """Extracts all log contents within log files
@@ -23,6 +28,48 @@ def extract_logs(log_file_paths: list[str]) -> list[str]:
     return all_logs
 
 
+def sort_log_files(log_paths: list[str]) -> list[str]:
+    """Sorts log files based on creation time
+
+    Parameters
+    ----------
+    f_paths : list[str]
+        list of all logs files
+
+    Returns
+    -------
+    list[str]
+        returns a list of log paths sorted based on creation
+    """
+    # collectime time created
+    file_time = []
+    for log_path in log_paths:
+        log_path_obj = Path(log_path)
+        mod_time = log_path_obj.stat().st_mtime
+        time = datetime.fromtimestamp(mod_time)
+        path_str = str(log_path_obj.absolute())
+        file_time.append((time, path_str))
+
+    file_time_df = pd.DataFrame(file_time, columns=["time_created", "path"])
+    file_time_df["time_created"] = pd.to_datetime(file_time_df["time_created"])
+    file_time_df = file_time_df.sort_values("time_created", ascending=True)
+    sorted_paths = file_time_df["path"].tolist()
+    return sorted_paths
+
+
+def get_all_logs() -> list[str]:
+    """Retuns all logs relative paths
+
+    Returns
+    -------
+    list[str]
+        relative paths of logs
+    """
+    log_rel_paths = glob.glob("./logs/*")
+    log_abs_path = [str(Path(rel_path).absolute()) for rel_path in log_rel_paths]
+    return log_abs_path
+
+
 def write_merge_logs(logs: list[str], outname: str) -> None:
     """Merges all logs into one file
 
@@ -39,31 +86,19 @@ def write_merge_logs(logs: list[str], outname: str) -> None:
     None
         Creates merged log file
     """
+    # sort logs by time created
+    sorted_logs = sort_log_files(logs)
 
     # convert list of logs into dataframe
     # sorting based on timestamp
     # convert sorted pandas df into list of log entries
     column_names = ["datetime", "debugg_type", "thread_id", "message"]
-    log_df = pd.DataFrame(data=logs, columns=column_names)
-    log_df["datetime"] = pd.to_datetime(log_df["datetime"], errors="coerce")
-    log_df = log_df.sort_values("datetime", ascending=True)
-    log_df["datetime"] = log_df["datetime"].astype(str)
+
+    conts = extract_logs(sorted_logs)
+    log_df = pd.DataFrame(data=conts, columns=column_names)
 
     # write log file
     with open(outname, "w") as outlog:
         for row_data in log_df.values.tolist():
             log_str = " ".join(row_data) + "\n"
             outlog.write(log_str)
-
-if __name__ in "__main__":
-
-    # getting all logs and extracting all contents
-    log_files = list(snakemake.input)
-    outname = str(snakemake.output)
-
-    logs = extract_logs(log_file_paths=log_files)
-    write_merge_logs(logs=logs, outname=outname)
-
-
-
-
