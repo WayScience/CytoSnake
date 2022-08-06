b7e13840c12041e114e5eca5cf419727118bb432
axiomcura <erikishere3@gmail.com>
2022-08-06 17:39:58 -0600

    edit docs

--- cytopipe/cli/cmd.py
+++ cytopipe/cli/cmd.py
@@ -10,7 +10,7 @@ import shutil
 from pathlib import Path
 
 from .args import *
-from .cli_checker import cli_check
+from .cli_checker import cli_check, _check_mode_help_arg
 from .exec.workflow_exec import exec_preprocessing
 from .cli_docs import cli_docs
 
@@ -27,59 +27,64 @@ def run_cmd() -> None:
     -------
     None
     """
+    # create args handler
+    # -- Cli Control Panel
+    args_handler = CliControlPanel(sys.argv)
+    print(args_handler.__repr__())
 
-    # checking provided cli arguments
-    cli_check(args_list=sys.argv)
+    # checking is user wanted to cli help
+    if args_handler.cli_help is True:
+        print(cli_docs)
+        sys.exit(0)
 
-    # collecting parameters
-    params = sys.argv[1:]
-    mode_type = params[0]
+    match args_handler.mode:
 
-    # parsing arguments based on modes
-    match mode_type:
         case "init":
 
-            # parsing inputs
-            init_args = parse_init_args(params)
+            if args_handler.mode_help is True:
+                print(init_doc)
+                sys.exit(0)
 
-            # setting up file paths
-            barcode_path = str(Path(init_args.barcode).absolute())
-            metadata_path = str(Path(init_args.metadata).absolute())
+            init_args = args_handler.parse_init_args()
 
-            # create data folder
-            # raises error if data directory exists (prevents overwriting)
-            data_dir_obj = Path("data")
-            data_dir_obj.mkdir(exist_ok=False)
-            data_dir_path = str(data_dir_obj.absolute())
+            # # setting up file paths
+            # barcode_path = str(Path(init_args.barcode).absolute())
+            # metadata_path = str(Path(init_args.metadata).absolute())
 
-            # moving all input files
-            for data_file in init_args.data:
-                f_path = str(Path(data_file).absolute())
-                shutil.move(f_path, data_dir_path)
+            # # create data folder
+            # # raises error if data directory exists (prevents overwriting)
+            # data_dir_obj = Path("data")
+            # data_dir_obj.mkdir(exist_ok=False)
+            # data_dir_path = str(data_dir_obj.absolute())
 
-            shutil.move(barcode_path, data_dir_path)
-            shutil.move(metadata_path, data_dir_path)
+            # # moving all input files
+            # for data_file in init_args.data:
+            #     f_path = str(Path(data_file).absolute())
+            #     shutil.move(f_path, data_dir_path)
 
-        case "run":
-            # selecting workflow process
-            proc_sel = params[1]
+            # shutil.move(barcode_path, data_dir_path)
+            # shutil.move(metadata_path, data_dir_path)
 
-            # executing process
-            if proc_sel == "cp_process":
-                cp_process_args = parse_cp_process_args(params)
+#         case "run":
+#             # selecting workflow process
+#             proc_sel = params[1]
 
-                print("executing cell profiler preprocessing")
-                status = exec_preprocessing(n_cores=cp_process_args.max_cores)
+#             # executing process
+#             if proc_sel == "cp_process":
+#                 cp_process_args = parse_cp_process_args(params)
 
-                # CLI exit based on status
-                if status:
-                    sys.exit(0)
-                else:
-                    sys.exit("Workflow unsuccessful")
+#                 print("executing cell profiler preprocessing")
+#                 status = exec_preprocessing(n_cores=cp_process_args.max_cores)
 
-        # display documentation of all modes
-        case "help":
-            print(cli_docs)
+#                 # CLI exit based on status
+#                 if status:
+#                     sys.exit(0)
+#                 else:
+#                     sys.exit("Workflow unsuccessful")
+
+#         # display documentation of all modes
+#         case "help":
+#             print(cli_docs)
 
 
 if __name__ == "__main__":
