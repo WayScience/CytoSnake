3da51e855c05f4c7ae1c6f7aab9d447c6ffc9234
axiomcura <erikishere3@gmail.com>
2023-01-13 11:38:56 -0700

    update project setup env

--- cytosnake/utils/cytosnake_setup.py
+++ cytosnake/utils/cytosnake_setup.py
@@ -7,22 +7,32 @@ are being conducted.
 It will be in charge of setting up path config files in order for cytosnake 
 
 """
+import json
 import shutil
-from pathlib import Path 
-from cytosnake.utils.cyto_paths import get_cytosnake_package_path, get_project_root
+
+from pathlib import Path
+from cytosnake.utils.cyto_paths import (
+    get_cytosnake_package_path,
+    get_project_root,
+    get_config_fpaths,
+    get_workflow_fpaths,
+    get_project_dirpaths,
+)
+
 from cytosnake.common.errors import display_error
 
+
 def create_cytosnake_dir() -> None:
-    """Sets up a `.cytosnake` folder that contains meta information that helps 
-    cytosnake's cli functionality. This will include configurations that 
+    """Sets up a `.cytosnake` folder that contains meta information that helps
+    cytosnake's cli functionality. This will include configurations that
     contains states, paths and settings.
-    
+
 
     Returns
     -------
     None
         Generates a `.cytosnake` file in current directory
-    
+
     Raises:
     FileExistsError
         Raised by pathlib.Path if `.cytosnake' exists in cur
@@ -39,8 +49,9 @@ def create_cytosnake_dir() -> None:
         display_error(e, msg=msg)
 
 
-def transport_files() -> None:
-    """Obtains the necessary files from software package and transport them into
+# NOTE: This should be in the `file_utils` module, may trigger circular imports
+def transport_project_files() -> None:
+    """obtains the necessary files from software package and transport them into
     current working directory"""
 
     # project directory
@@ -50,7 +61,7 @@ def transport_files() -> None:
     pkg_path = get_cytosnake_package_path()
 
     # get all important folders
-    # - workflows: contains all workflow files (envs, rules) 
+    # - workflows: contains all workflow files (envs, rules)
     # - configs: contains yaml files providing workflow and cli configurations
     target_dirs = ["workflows", "configs"]
     for target_dir in target_dirs:
@@ -64,44 +75,43 @@ def transport_files() -> None:
         shutil.copytree(src_path, proj_path)
 
 
-# TODO: finish this after figuring out on how to search for files in directories
+# todo: finish this after figuring out on how to search for files in directories
 def generate_meta_path_configs() -> None:
-    """Constructs a `_paths.yaml` file that contains the necessary path 
-    information for file handling. This allows `cytosnake` to know which folders
+    """constructs a `_paths.yaml` file that contains the necessary path
+    information for file handling. this allows `cytosnake` to know which folders
     to look for when performing tasks.
 
-    Returns
+    returns
     -------
-    None
-        Creates a `_paths.yaml` file in the `.cytosnake` project directory
+    none
+        creates a `_paths.yaml` file in the `.cytosnake` project directory
     """
-    # create `_path.yaml` file in side `.cytosnake` directory
-    cytosnake_dir = get_project_root() / ".cytosnake"
-    create_path = cytosnake_dir / "_paths.yaml"
 
-    # setting up stream to write data into `_paths.yaml`
-    with open(create_path, "w") as stream:
+    # get all pathing info
+    dir_paths = {"project_dir": get_project_dirpaths()}
+    config_fpaths = {"config_dir": get_config_fpaths()}
+    workflow_fpath = {"workflow_dir": get_workflow_fpaths()}
 
-        # Collect all paths from `workflow` and `configs` directory
-        return stream
+    # merge all path dictionaries into one
+    all_paths_dict = dir_paths | config_fpaths | workflow_fpath
+
+    # write dict into yaml file
+    save_path = get_project_root() / ".cytosnake" / "_paths.yaml"
+    with open(save_path, "w") as stream:
+        json.dump(all_paths_dict, stream, indent=4)
 
 
 def setup_cytosnake_env() -> None:
-    """ Main wrapper function that sets up current directory into a `cytosnake`
-    project directory. This means that all the analysis being conducted within
-    the current directory will allow 
+    """main wrapper function that sets up current directory into a `cytosnake`
+    project directory. this means that all the analysis being conducted within
+    the current directory will allow
     """
 
     # create '.cytosnake' file
     create_cytosnake_dir()
 
     # move necessary files to directory
-    transport_files()
+    transport_project_files()
 
-
-
-
-
-
-
-    
+    # create `_paths.yaml` path meta data in `.cytosnake` project dir
+    generate_meta_path_configs()
