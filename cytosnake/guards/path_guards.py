4a581282b6dd874fd5f430e22a99b982b1ecd7ad
axiomcura <erikishere3@gmail.com>
2023-01-13 11:38:41 -0700

    new modele `path_guards`

---
+++ cytosnake/guards/path_guards.py
@@ -0,0 +1,29 @@
+"""
+Module: path_guards.py
+
+
+Contains functions that ensures that types that are considered paths are
+checked. 
+
+This includes:
+    - existing paths 
+    - valid path strings 
+"""
+
+from typing import TypeGuard
+from pathlib import Path
+
+
+def is_valid_path(val: object) -> TypeGuard[Path]:
+    """checks if provided value is a valid path"""
+
+    # check if the val is valid type
+    # -- if string, convert to Path
+    accepted_types = (str, Path)
+    if not isinstance(val, accepted_types):
+        return False
+    if isinstance(val, str):
+        val = Path(val)
+
+    # check if the path exists
+    return bool(val.exists())
