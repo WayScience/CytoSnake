28a80ef091a430581c53f3fa1220c07adaf867e1
axiomcura <erikishere3@gmail.com>
2023-01-24 11:28:04 -0700

    helper functions added

--- cytosnake/helpers/helper_funcs.py
+++ cytosnake/helpers/helper_funcs.py
@@ -1,4 +1,13 @@
-from cytosnake.utils.cyto_paths import get_data_path
+"""
+Module: helper_funcs.py
+
+This module contains helper functions for snakemake workflows to access with. 
+
+"""
+
+
+from pathlib import Path
+from cytosnake.utils.config_utils import load_meta_path_configs
 
 
 def get_data() -> str:
@@ -7,14 +16,25 @@ def get_data() -> str:
     Returns
     -------
     str
-        returns a string that contains a snakemake wild card that collects all 
+        returns a string that contains a snakemake wild card that collects all
         plate data
-        
+
+    Raises
+    ------
+    FileNotFoundError
+        Raised if the data folder is not found in the current project folder
     """
 
-    # get the datapath
-    data_path = get_data_path()
-    return str(data_path) + "/{plate_name}.sqlite"
+    # load in the meta_path configs
+    meta_configs = load_meta_path_configs()
+    data_dir_path = Path(meta_configs["project_dir"]["data"])
+    print(data_dir_path)
 
+    # check if the path exists
+    if not data_dir_path.exists():
+        raise FileNotFoundError(
+            "Unable to find data path in project directory"
+        )
 
-# def get_metadata_dir 
+    # return snakemake format string path
+    return str(data_dir_path / "{file_name}.sqlite")
