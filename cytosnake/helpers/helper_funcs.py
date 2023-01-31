c72eef1162d779dc9082bbbf59db23c43230b910
axiomcura <erikishere3@gmail.com>
2023-01-31 11:42:52 -0700

    added input helper function

--- cytosnake/helpers/helper_funcs.py
+++ cytosnake/helpers/helper_funcs.py
@@ -9,8 +9,29 @@ This module contains helper functions for snakemake workflows to access with.
 from pathlib import Path
 from cytosnake.utils.config_utils import load_meta_path_configs
 
+def get_data_folder() -> Path:
+    """_summary_
 
-def get_data() -> str:
+    Returns
+    -------
+    Path
+        _description_
+
+    Raises
+    ------
+    KeyError
+        Raised if the `data` path is not found in the `_paths.yaml`
+        
+    """
+    meta_configs = load_meta_path_configs()
+    try:
+        data_dir_path = Path(meta_configs["project_dir"]["data"])
+    except KeyError:
+        raise KeyError("Unable to find data folder path")
+
+    return data_dir_path
+
+def get_plate_data() -> str:
     """Returns absolute path were the data is located
 
     Returns
@@ -19,22 +40,32 @@ def get_data() -> str:
         returns a string that contains a snakemake wild card that collects all
         plate data
 
-    Raises
-    ------
-    FileNotFoundError
-        Raised if the data folder is not found in the current project folder
     """
-
     # load in the meta_path configs
-    meta_configs = load_meta_path_configs()
-    data_dir_path = Path(meta_configs["project_dir"]["data"])
-    print(data_dir_path)
-
-    # check if the path exists
-    if not data_dir_path.exists():
-        raise FileNotFoundError(
-            "Unable to find data path in project directory"
-        )
-
-    # return snakemake format string path
+    data_dir_path = get_data_folder()
     return str(data_dir_path / "{file_name}.sqlite")
+
+
+def get_barcodes() -> str:
+    """Obtains path to the bardcode file within data folder
+
+    Returns
+    -------
+    str
+        return string with barcode name wildcard
+        
+    """
+    data_dir_path = get_data_folder()
+    return str(data_dir_path / "{barcode_name}.txt")
+
+def get_metadata_dir() -> str:
+    """Obtains path to the metadata folder with in data folder
+
+    Returns
+    -------
+    str
+        returns tring that contains metdata wildcard
+    """
+    data_dir_path = get_data_folder()
+    return str(data_dir_path / "{metadata}")
+
