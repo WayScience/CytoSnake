5ba84216ec7154ad86db30e46bce7042e38acdb3
axiomcura <erikishere3@gmail.com>
2023-05-07 09:15:13 -0600

    added testing framework

--- cytosnake/tests/functional/test_cli.py
+++ cytosnake/tests/functional/test_cli.py
@@ -14,9 +14,64 @@ Ultimately, test_cli.py will contains functional test to all modes that CytoSnak
 contains.
 """
 
+import pathlib
+from dataclasses import dataclass
+from typing import Optional
+
 # import tempfile
 # import pytest
 # import subprocess
+# import test_functions # This will contains helper functions for testing
+
+
+# -----------------
+# Helper functions
+# -----------------
+@dataclass
+class DummyInputs:
+    platedata: str
+    metadata: str
+    barcode: Optional[str] = None
+
+
+def generate_input_paths(
+    n_plates: int, n_platemaps: int, barcode: Optional[bool] = True
+) -> DummyInputs:
+    """Generates DummyInputs object that contains platedata, barcode and metadata paths.
+    These paths point to dummy
+
+    Parameters
+    ----------
+    n_plates : int
+        number of dummy sqlite files
+    n_platemaps : int
+        number of dummy plate map files
+    barcode : Optional[bool], optional
+        True if a dummy barcode should be added into the DummyInputs, Else false
+        , by default True
+    """
+
+    # getting root dir where all the dummy data is stored
+    root_dummy_data_dir = pathlib.Path("./datasets/dummyfiles").resolve(strict=True)
+
+    # now generated the amount of dummy data based on parameters
+    # getting plate data
+    plates_list = list(root_dummy_data_dir.glob("*.sqlite"))[: n_plates + 1]
+    plates_list_str = " ".join(str(_path.absolute()) for _path in plates_list)
+
+    # getting plate maps
+    platemaps_dir = root_dummy_data_dir / "metadata" / "platemap"
+    platemaps_list = list(platemaps_dir.glob("platemap*"))[: n_platemaps + 1]
+    platemaps_list_str = " ".join(str(_path.absolute()) for _path in platemaps_list)
+
+    # getting barcode
+    if barcode is True:
+        barcode_path = str(root_dummy_data_dir / "barcode.txt")
+    else:
+        barcode_path = None
+
+    return DummyInputs(plates_list_str, platemaps_list_str, barcode_path)
+
 
 # --------------------------
 # init mode functional tests
@@ -30,7 +85,8 @@ def test_barcode_logic_no_barcode_one_platemap() -> None:
     , this means that the generated dataset came from one experiment and multiple
     samples (plates) were used to generated the datasets.
     """
-    pass
+    # generating input strings
+    _inputs = generate_input_paths(n_plates=3, n_platemaps=1, barcode=False)
 
 
 def test_barcode_logic_barcode_multi_platemaps() -> None:
@@ -39,7 +95,10 @@ def test_barcode_logic_barcode_multi_platemaps() -> None:
     , this means that the generated dataset came from one experiment and multiple
     samples (plates) were used to generated the datasets.
     """
-    pass
+    # generating input strings
+    _inputs = generate_input_paths(n_plates=3, n_platemaps=2, barcode=True)
+
+    # execute cli
 
 
 def test_barcode_logic_no_barcode_multi_platemaps() -> None:
@@ -48,4 +107,7 @@ def test_barcode_logic_no_barcode_multi_platemaps() -> None:
     there are plate maps, this indicates that the generated datasets came from multiple
     experiments.
     """
-    pass
+    # generating input strings
+    _inputs = generate_input_paths(n_plates=3, n_platemaps=2, barcode=False)
+
+    # execute cli
