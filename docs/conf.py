7e66d412fabcdc991c35f7ac2aed56a785d90a54
axiomcura <erikishere3@gmail.com>
2023-06-12 19:55:27 -0600

    updated to furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,8 @@ release = "0.0.1"
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "myst_parser"]
 
+# HTML themes
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
@@ -27,5 +29,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
