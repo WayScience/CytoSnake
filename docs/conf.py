ae95d7a6c9b97a0f2543f9ac619a7840b408e726
axiomcura <erikishere3@gmail.com>
2023-08-22 14:36:04 -0600

    update

--- docs/conf.py
+++ docs/conf.py
@@ -30,5 +30,4 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-# html_static_path = ["_static"]
 html_logo = "../logo/just-icon.svg"
